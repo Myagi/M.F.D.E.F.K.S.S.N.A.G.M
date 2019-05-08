c3f1b065e575b1e95b2f7324b3a91ec6f5af7ca1
Ben Blumenfeld <ben.blumenfeld@myagi.com>
2019-05-07 19:55:00 -0700

    feed ready

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import Search from './components/Search.jsx';
-import RepoList from './components/RepoList.jsx';
+import FeedItems from './components/FeedItems';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      repos: []
+
     }
 
   }
 
-  search (term) {
-    console.log(`${term} was searched`);
-    // TODO
-  }
 
   render () {
-    return (<div>
-      <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
-    </div>)
+    return (
+      <div>
+        <FeedItems />
+      </div>
+    )
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -3,19 +3,15 @@ let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+app.post('/feed', function (req, res) {
+  //TODO:
 });
 
-app.get('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should send back the top 25 repos
+app.get('/feed', function (req, res) {
+  //TODO:
 });
 
-let port = 1128;
+let port = 8080;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
